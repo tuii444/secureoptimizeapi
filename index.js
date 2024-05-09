3419ad3316710fdc298c8be97a2147451758aaa4
tuii444 <silsileyimpun0suy@hotmail.com>
2024-05-09 07:16:54 +0700

    update task

--- index.js
+++ index.js
@@ -1,2 +1,7 @@
-const nthElement = (arr, n = 0) =>
-  (n > 0 ? arr.slice(n, n + 1) : arr.slice(n))[0];
+const fibonacci = (n) => {
+  const sequence = [0, 1];
+  for (let i = 2; i <= n; i++) {
+    sequence.push(sequence[i - 1] + sequence[i - 2]);
+  }
+  return sequence;
+};
